b8443d08cd1f274eac40107bdbe50472f5006a2e
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-10-10 14:09:15 +0700

    ok

--- src/dbs/dbs.connect.js
+++ src/dbs/dbs.connect.js
@@ -2,7 +2,7 @@
 
 const {default: mongoose} = require('mongoose')
 
-const connectString = process.env.DB_URL 
+const connectString = 'mongodb+srv://dauhu1232019:Anhyeuem28@cluster01.kmae3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01'
 
 mongoose.connect(connectString)
     .then(_=> console.log("Connect MongoDB successfully!!"))
